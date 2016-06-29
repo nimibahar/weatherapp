88d08ba0c8da8cd09ab977ef778f89847208c8e8
Nimrod Bahar <nimrodbahar@gmail.com>
2016-06-29 10:31:20 +0200

    Add links to example

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,11 +1,19 @@
 var React = require('react');
-
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to the exapmles page</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example location to try out:</p>
+        <ol>
+          <li>
+            <Link to='/?location=Paris'>Paris</Link>
+          </li>
+          <li>
+            <Link to='/?location=Tel-Aviv'>Tel-Aviv</Link>
+          </li>
+        </ol>
     </div>
   );
 }
